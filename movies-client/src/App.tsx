9d4ba6290587b78c65247064b50505288c56e74f
Junior Huamaní <junior.r.huamani@gmail.com>
2022-03-06 11:23:51 -0300

    Updating project

---
+++ movies-client/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Holaaa</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        minus itaque saepe nesciunt at quibusdam alias modi sint eveniet
+        repellat doloremque, eligendi consectetur accusamus ipsa? Possimus
+        maiores unde neque reiciendis!
+      </p>
+      <Button colorScheme="blue">Button</Button>
+    </React.Fragment>
+  );
+}
+
+export default App;
