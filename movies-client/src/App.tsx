902190b488979cf561f511f653bf9b36700fab24
Junior Huamaní <junior.r.huamani@gmail.com>
2022-04-26 19:55:03 -0300

    New squeme

--- movies-client/src/App.tsx
+++ movies-client/src/App.tsx
@@ -3,9 +3,14 @@ import React from "react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
+  const most = (n: number) => (n * (n + 1)) / 2;
+
   return (
     <React.Fragment>
       <h1>Holaaa</h1>
+      <p>
+        The most number of {most(10)} is {most(10)}
+      </p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
         minus itaque saepe nesciunt at quibusdam alias modi sint eveniet
